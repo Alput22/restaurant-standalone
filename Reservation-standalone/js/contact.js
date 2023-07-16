0590195b7b2e2aa68462472d5a25efba35daae68
Almada Putra <55756884+Alput22@users.noreply.github.com>
2023-07-16 15:26:55 +0700

    Add files via upload

---
+++ Reservation-standalone/js/contact.js
@@ -0,0 +1,14 @@
+Vue.component('contact',
+{
+    props: ["id"],
+    template: `<div: class="'animated fadeInRight component contact' + id">
+                <i class="material-icons-two-tone">phone</i>
+                <span>08118896172</span><hr class="hrhr">
+
+                <i class="material-icons-two-tone">email</i>
+                <span>demo@gmail.com</span><hr class="hrhr">
+
+                <i class="material-icons-two-tone">location</i>
+                <span>Omah Pawon</span><hr class="hrhr">
+                </div>`
+})
---
+++ Reservation-standalone/vue.config.js
@@ -0,0 +1,7 @@
+// vue.config.js
+module.exports = {
+    devServer: {
+      proxy: 'https://reservation-standalone-default-rtdb.asia-southeast1.firebasedatabase.app',
+    }
+  }
+  
